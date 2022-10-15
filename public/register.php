ccd68e3bf97a98c2f1791ebf95f0a1960f6a8b1f
Yasir Abbas <yasir4abas@outlook.com>
2022-10-15 09:32:23 +0500

    Update register.php

--- public/register.php
+++ public/register.php
@@ -94,10 +94,10 @@ if (isset($_POST['verify'])) {
         mysqli_select_db($con, $radius_db_name);
         mysqli_query($con,"INSERT INTO `radcheck` (`username`, `attribute`, `op`, `value`) VALUES ('$reg', 'Cleartext-Password', ':=', '$otp')");
 
-        header("Location: index.php");
+        // header("Location: index.php");
+    } else {
+        $user_error = 2;
     }
-    $user_error = 2;
-
     // TODO: redirect to index page
   } else {
     // User not found, display error
